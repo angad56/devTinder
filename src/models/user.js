a6e4b85b93f4da14fd622e160033b1cbb9ba497f
angad56 <angadkapoor56@gmail.com>
2024-10-13 02:44:42 +0530

    installed validator and added validator func

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,6 +18,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address" + value);
+        }
+      },
     },
     password: {
       type: String,
@@ -38,6 +44,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://www.mitconskills.com/wp-content/uploads/2022/06/dummy-user.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL" + value);
+        }
+      },
     },
     about: {
       type: String,
