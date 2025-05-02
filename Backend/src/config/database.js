e4334857619b163716d93fba08e7704b21961532
Angad Kapoor <122010895+angad56@users.noreply.github.com>
2025-05-03 00:56:24 +0530

    Update database.js

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://angadkapoor56:Bunnyknows123$@cluster0.j5lardt.mongodb.net/devTinder"
+    "mongodb+srv://angadkapoor56:Bunnyknows12345@cluster0.j5lardt.mongodb.net/devTinder"
   );
 };
 
